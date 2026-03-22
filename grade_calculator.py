338d7aad945d95f741d09e4909de4a7594c86a60
KarmegamJ <jkarmegam657@gmail.com>
2026-03-22 22:01:38 +0530

    fix: code added

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,35 +1,37 @@
-while True:
-    try:
-        Student_name = input("Enter Student Name: ")
-        Enter_marks = int(input("Enter marks (0-100):"))
+def calculate_grade():
+    student_name = input("Enter Student Name: ")
+    while True:
+        try:
 
-        if 0<= Enter_marks <= 100:
-            print(f"📊 RESULT FOR {Student_name}:")
-            print(f"Marks: {Enter_marks}/100")
+            enter_marks = int(input("Enter marks (0-100):"))
 
-            if 90 <= Enter_marks <= 100:
-                print("Grade:A ")
-                print("Excellent work! Very good! Keep it up! 🌟")
+            if 0<= enter_marks <= 100:
+                print(f"📊 RESULT FOR {student_name}:")
+                print(f"Marks: {enter_marks}/100")
 
-            elif 80<= Enter_marks <= 89:
-                print("Grade:B ")
-                print("Great job! You're doing really well—keep improving! 👍")
+                if 90 <= enter_marks <= 100:
+                    print("Grade:A ")
+                    print("Excellent work! Very good! Keep it up! 🌟")
 
-            elif 70 <= Enter_marks <= 79:
-                print("Grade:C ")
-                print("Good effort! Keep practicing and you’ll get even better! 📈")
+                elif 80<= enter_marks <= 89:
+                    print("Grade:B ")
+                    print("Great job! You're doing really well—keep improving! 👍")
 
-            elif 60 <= Enter_marks <= 69:
-                print("Grade:D ")
-                print("Fair attempt! Put in a bit more effort to improve! 💪")
+                elif 70 <= enter_marks <= 79:
+                    print("Grade:C ")
+                    print("Good effort! Keep practicing and you’ll get even better! 📈")
 
-            else:
-                print("Grade:F ")
-                print("Needs improvement. Don’t give up—try again with more focus! 🔄")
+                elif 60 <= enter_marks <= 69:
+                    print("Grade:D ")
+                    print("Fair attempt! Put in a bit more effort to improve! 💪")
 
+                else:
+                    print("Grade:F ")
+                    print("Needs improvement. Don’t give up—try again with more focus! 🔄")
                 break
 
-        else:
-            print("❌ Invalid marks! Please enter between 0 and 100.\n")
-    except ValueError:
-        print("Invalid context, type ins number")
+            else:
+                print("❌ Invalid marks! Please enter between 0 and 100.\n")
+        except ValueError:
+            print("Invalid context, type in number")
+calculate_grade()
